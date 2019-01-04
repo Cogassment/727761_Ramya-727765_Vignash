91f9c69794713a5b7924fe0565b367266572876d
Cogassment <46102794+Cogassment@users.noreply.github.com>
2019-01-04 18:32:45 +0530

    Add files via upload

---
+++ RlgBilling/Controllers/BillingController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using RlgBilling.Models;
+using System.Configuration;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Web.UI;
+
+namespace RlgBilling.Controllers
+{
+    public class BillingController : Controller
+
+    {
+        // GET: Billing
+        public ActionResult Index()
+        {
+            return View(GetBillingDetails());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(BillingModel billingModel)
+
+        {
+            if (ModelState.IsValid)
+            {
+
+                string constr = ConfigurationManager.ConnectionStrings["Constring"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    string getQuery= "select*from BillingTable where AssociateID =" + billingModel.AssociateID;
+                    string query = "Insert into BillingTable(ProjectID,ProjectName,ManagerName,AssociateID,AssociateName,AllocationPercent,OnOff,RateCard,BillableDays,LeaveDays,Amount,Comments)VALUES(@ProjectID,@ProjectName,@ManagerName,@AssociateID,@AssociateName,@AllocationPercent,@OnOff,@RateCard,@BillableDays,@LeaveDays,@Amount,@Comments)";
+
+                    using (SqlCommand cmd = new SqlCommand(query))
+                    {
+                        cmd.Connection = con;
+                        con.Open();
+                        cmd.Parameters.AddWithValue("@ProjectID", billingModel.ProjectID);
+                        cmd.Parameters.AddWithValue("@ProjectName", billingModel.ProjectName);
+                        cmd.Parameters.AddWithValue("@ManagerName", billingModel.ManagerName);
+                        cmd.Parameters.AddWithValue("@AssociateID", billingModel.AssociateID);
+                        cmd.Parameters.AddWithValue("@AssociateName", billingModel.AssociateName);
+                        cmd.Parameters.AddWithValue("@AllocationPercent", billingModel.AllocationPercent);
+                        cmd.Parameters.AddWithValue("@OnOff", billingModel.OnOff);
+                        cmd.Parameters.AddWithValue("@RateCard", billingModel.RateCard);
+                        cmd.Parameters.AddWithValue("@BillableDays", billingModel.BillableDays);
+                        cmd.Parameters.AddWithValue("@LeaveDays", billingModel.LeaveDays);
+                        cmd.Parameters.AddWithValue("@Amount", billingModel.Amount);
+                        cmd.Parameters.AddWithValue("@Comments", billingModel.Comments);
+                        cmd.ExecuteNonQuery();
+
+                        con.Close();
+
+
+                    }
+
+                }
+                ViewBag.message = "User Saved successfully";
+                ModelState.Clear();
+
+
+                //return Content("Saved successfully");
+
+            }
+            return View();
+            //else
+            //{
+            //    ViewBag.message = "Enter a valid data!!!!";
+            //    return View(); //}
+
+        }
+
+
+        [HttpPost]
+        public ActionResult Save(int BillableDays, int LeaveDays, int Amount, int AssociateID, string Comments)
+        {
+
+
+            string query = "update BillingTable set BillableDays = '" + BillableDays + "', LeaveDays = " + LeaveDays + " , Amount = '" + Amount + "',Comments = '" + Comments + "' where AssociateID =" + AssociateID;
+            string constr = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\727765\source\repos\RlgBilling\RlgBilling\App_Data\RlgDb.mdf; Integrated Security = True";
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Connection = con;
+
+                    cmd.ExecuteNonQuery();
+
+                }
+                con.Close();
+
+            }
+
+            return Json("Details saved successfully.");
+        }
+
+
+
+        private IList<BillingModel> GetBillingDetails()
+        {
+            SqlConnection con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\727765\source\repos\RlgBilling\RlgBilling\App_Data\RlgDb.mdf; Integrated Security = True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from BillingTable", con);
+            cmd.ExecuteNonQuery();
+            List<BillingModel> list = new List<BillingModel>();
+            BillingModel m;                                                       /* m=object*/
+            int r = 0;                                                            /*r=Localvariable*/
+            using (SqlDataReader read = cmd.ExecuteReader())
+            {
+                while (read.Read())
+                {
+                    m = new BillingModel();
+                    m.ProjectID = int.Parse(read["ProjectID"].ToString());
+                    m.ProjectName = read["ProjectName"].ToString();
+                    m.ManagerName = read["ManagerName"].ToString();
+                    m.AssociateID = int.Parse(read["AssociateID"].ToString());
+                    m.AssociateName = read["AssociateName"].ToString();
+                    m.AllocationPercent = int.Parse(read["AllocationPercent"].ToString());
+                    m.OnOff = read["OnOff"].ToString();
+                    m.RateCard = int.Parse(read["RateCard"].ToString());
+                    m.BillableDays = int.Parse(read["BillableDays"].ToString());
+                    r = m.RateCard * m.BillableDays;
+                    m.LeaveDays = int.Parse(read["LeaveDays"].ToString());
+                    m.Amount = Convert.ToInt32(r);
+                    m.Amount = r;
+                    m.Comments = read["Comments"].ToString();
+
+                    list.Add(m);
+
+                }
+                con.Close();
+            }
+
+            return list;
+        }
+        public ActionResult ExportToExcel()
+        {
+            var data = GetBillingDetails();
+            var gv = new GridView();                                /*gv=gridview*/
+            gv.DataSource = this.GetBillingDetails();
+            gv.DataBind();
+            Response.ClearContent();
+            Response.Buffer = true;
+            Response.AddHeader("content-disposition", "attachment; filename=BillingDetails.xls");
+            Response.ContentType = "application/ms-excel";
+            Response.Charset = "";
+            StringWriter objStringWriter = new StringWriter();
+            HtmlTextWriter objHtmlTextWriter = new HtmlTextWriter(objStringWriter);
+            gv.RenderControl(objHtmlTextWriter);
+            Response.Output.Write(objStringWriter.ToString());
+            Response.Flush();
+            Response.End();
+            return Content("Success");
+        }
+
+    }
+
+}
+
+
+
---
+++ RlgBilling/Models/BillingModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RlgBilling.Models
+{
+    public class BillingModel
+    {
+
+        public int ProjectID { get; set; }
+
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = " Only Alphabets are allowed")]
+        public string ProjectName { get; set; }
+
+        [RegularExpression("^[a-zA-Z, ]*$", ErrorMessage = " Only Alphabets are allowed")]
+        public string ManagerName { get; set; }
+
+        [RegularExpression("^[0-9]{6}$", ErrorMessage = "Please Enter Valid AssociateID with 6 characters")]
+        public int AssociateID { get; set; }
+
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = " Only Alphabets are allowed")]
+        public string AssociateName { get; set; }
+
+        public int AllocationPercent { get; set; }
+
+        [RegularExpression("^[O-ON-NF-F ]{2}$", ErrorMessage = " Enter ON or OF")]
+        public string OnOff { get; set; }
+
+        [Required(ErrorMessage = "values is required.")]
+        public int RateCard { get; set; }
+
+        [Required(ErrorMessage = "values is required.")]
+        public int BillableDays { get; set; }
+        public int LeaveDays { get; set; }
+        public int Amount { get; set; }
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = " Only Alphabets are allowed")]
+        public string Comments { get; set; }
+    }
+}
