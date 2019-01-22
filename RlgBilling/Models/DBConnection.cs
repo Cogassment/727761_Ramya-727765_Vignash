4510573721ea18ebd1b0ad17ab396e6562f7d27f
Cogassment <46102794+Cogassment@users.noreply.github.com>
2019-01-22 15:17:04 +0530

    Add files via upload

---
+++ RlgBilling/Models/DBConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace RlgBilling.Models
+{
+    public class DBConnection
+    {
+        public SqlConnection con()
+        {
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Constring"].ConnectionString);
+            return connection;
+        }
+    }
+}
